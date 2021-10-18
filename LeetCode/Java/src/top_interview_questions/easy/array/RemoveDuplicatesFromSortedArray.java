3f120239e98a5678191e114ac2548ba74a34a71b
Eric Wu <ericwoodenman@gmail.com>
2021-10-18 10:27:58 -0400

    Adjust class structure

--- LeetCode/Java/src/top_interview_questions/easy/array/RemoveDuplicatesFromSortedArray.java
+++ LeetCode/Java/src/top_interview_questions/easy/array/RemoveDuplicatesFromSortedArray.java
@@ -21,50 +21,50 @@ public class RemoveDuplicatesFromSortedArray {
         System.out.print(" ] \n");
         System.out.println(solution.removeDuplicates(nums));
     }
-}
 
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        int k = 0; // unique element amount
+    static class Solution {
+        public int removeDuplicates(int[] nums) {
+            int k = 0; // unique element amount
 
-        /*
-         * Solution v1: Two layer loop with double pointers.
-         * Runtime: 5 ms
-         * Memory Usage: 40.2 MB
-         *
-         * Summary: Runtime only beats 8.72%.
-         */
+            /*
+             * Solution v1: Two layer loop with double pointers.
+             * Runtime: 5 ms
+             * Memory Usage: 40.2 MB
+             *
+             * Summary: Runtime only beats 8.72%.
+             */
 
-        // for (int i = 0; i < nums.length; i++) {
-        //     boolean isUnique = true;
-        //     for (int j = 0; j < k; j++) {
-        //         if (nums[j] == nums[i]) {
-        //             isUnique = false;
-        //             break;
-        //         }
-        //     }
-        //     if (isUnique) {
-        //         nums[k] = nums[i];
-        //         k++;
-        //     }
-        // }
+            // for (int i = 0; i < nums.length; i++) {
+            //     boolean isUnique = true;
+            //     for (int j = 0; j < k; j++) {
+            //         if (nums[j] == nums[i]) {
+            //             isUnique = false;
+            //             break;
+            //         }
+            //     }
+            //     if (isUnique) {
+            //         nums[k] = nums[i];
+            //         k++;
+            //     }
+            // }
 
-        /*
-         * Solution v2: Single layer loop
-         * (Because given a sorted array)
-         * Runtime: 1 ms
-         * Memory Usage: 44.7 MB
-         *
-         * Summary: Runtime only beats 86.47%.
-         */
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                nums[k++] = nums[i];
-            } else if (nums[k - 1] < nums[i]) {
-                nums[k++] = nums[i];
+            /*
+             * Solution v2: Single layer loop
+             * (Because given a sorted array)
+             * Runtime: 1 ms
+             * Memory Usage: 44.7 MB
+             *
+             * Summary: Runtime only beats 86.47%.
+             */
+            for (int i = 0; i < nums.length; i++) {
+                if (i == 0) {
+                    nums[k++] = nums[i];
+                } else if (nums[k - 1] < nums[i]) {
+                    nums[k++] = nums[i];
+                }
             }
-        }
 
-        return k;
+            return k;
+        }
     }
 }
