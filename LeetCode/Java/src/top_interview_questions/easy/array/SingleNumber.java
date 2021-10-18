43a52a4c2377cde7bb93da8f8caef81998d34364
Eric Wu <ericwoodenman@gmail.com>
2021-10-18 18:48:23 -0400

    Add 2 solutions for "Single Number". Pay attention to the bitwise XOR.

--- LeetCode/Java/src/top_interview_questions/easy/array/SingleNumber.java
+++ LeetCode/Java/src/top_interview_questions/easy/array/SingleNumber.java
@@ -1,7 +1,5 @@
 package top_interview_questions.easy.array;
 
-import java.util.Arrays;
-
 public class SingleNumber {
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -35,40 +33,67 @@ public class SingleNumber {
             // }
 
             /*
-             * Solution v2: two dimensional Boolean array
+             * Solution v2: two Boolean arrays
              * 61 / 61 test cases passed
              *
              * Runtime: 1 ms @ (beats) 94.91%
              * Memory Usage: 38.8 MB @ (beats) 91.79%
              */
 
-            int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-            for (int i : nums) {
-                if (max < i) {
-                    max = i;
-                }
-                if (min > i) {
-                    min = i;
-                }
-            }
+            // int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+            // for (int i : nums) {
+            //     if (max < i) {
+            //         max = i;
+            //     }
+            //     if (min > i) {
+            //         min = i;
+            //     }
+            // }
+            //
+            // boolean[] a = new boolean[max - min + 1];
+            // boolean[] b = new boolean[max - min + 1];
+            //
+            // for (int i : nums) {
+            //     if (a[i - min]) {
+            //         b[i - min] = true;
+            //     } else {
+            //         a[i - min] = true;
+            //     }
+            // }
+            // for (int i = 0; i < b.length; i++) {
+            //     if (a[i] && !b[i]) {
+            //         System.out.println(i);
+            //         result = i + min;
+            //     }
+            // }
 
-            boolean[] a = new boolean[max - min + 1];
-            boolean[] b = new boolean[max - min + 1];
 
-            for (int i : nums) {
-                if (a[i - min]) {
-                    b[i - min] = true;
-                } else {
-                    a[i - min] = true;
-                }
-            }
-            for (int i = 0; i < b.length; i++) {
-                if (a[i] && !b[i]) {
-                    System.out.println(i);
-                    result = i + min;
-                }
-            }
+            /*
+             * Solution v3: HashSet
+             * 61 / 61 test cases passed
+             *
+             * Runtime: 18 ms @ (beats) 14.78%
+             * Memory Usage: 52.2 MB @ (beats) 0.0%
+             */
 
+            // HashSet<Integer> hashSet = new HashSet<>();
+            // for (int i : nums) {
+            //     if (hashSet.contains(i)) {
+            //         hashSet.remove(i);
+            //     } else {
+            //         hashSet.add(i);
+            //     }
+            // }
+            // result = hashSet.stream().findFirst().get();
+
+            /*
+             * (learn from Internet)
+             * Solution v4: bitwise XOR
+             */
+            result = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                result ^= nums[i];
+            }
 
             return result;
         }
