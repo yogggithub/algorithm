3b507adb9cc8a2903f27a18b726434f550e91314
Eric Wu <ericwoodenman@gmail.com>
2021-10-24 07:58:54 -0400

    Solution for "Delete Node in a Linked List"

---
+++ LeetCode/Java/src/top_interview_questions/easy/linkedlist/DeleteNode.java
@@ -0,0 +1,23 @@
+package top_interview_questions.easy.linkedlist;
+
+public class DeleteNode {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+
+    static class Solution {
+        public void deleteNode(ListNode node) {
+            /*
+             * Solution v1: replace this node with next node
+             *
+             * Runtime: 0 ms @ (beats) ??%
+             * Memory Usage: 40.6 MB @ (beats) 39.18%
+             */
+            node.val = node.next.val;
+            node.next = node.next.next;
+
+        }
+    }
+}
+
+
