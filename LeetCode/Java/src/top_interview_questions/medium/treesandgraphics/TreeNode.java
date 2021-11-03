d118ee4ad2ede86e35d8c057f6636eadeba179a6
Eric Wu <ericwoodenman@gmail.com>
2021-11-03 10:33:37 -0400

    Solution for "Binary Tree Inorder Traversal"

---
+++ LeetCode/Java/src/top_interview_questions/medium/treesandgraphics/TreeNode.java
@@ -0,0 +1,20 @@
+package top_interview_questions.medium.treesandgraphics;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
