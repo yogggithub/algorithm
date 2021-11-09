d04d0ac03110990ee195310440a14b02f55555c8
Eric Wu <ericwoodenman@gmail.com>
2021-11-09 08:17:06 -0500

    Make all method in ListNode and TreeNode classes public. And comment any unnecessary method

--- LeetCode/Java/src/auxiliary/ListNode.java
+++ LeetCode/Java/src/auxiliary/ListNode.java
@@ -6,39 +6,39 @@ public class ListNode {
     int val;
     ListNode next;
 
-    ListNode() {
+    public ListNode() {
     }
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
 
-    public static ListNode arrayToListNode(int[] nodeValues) {
-        ListNode dummyRoot = new ListNode(-1);
-        ListNode temp = dummyRoot;
-        for (int val : nodeValues) {
-            temp.next = new ListNode(val);
-            temp = temp.next;
-        }
-        return dummyRoot.next;
-    }
-
-    public static void print(ListNode node) {
-        ArrayList<Integer> vals = new ArrayList<>();
-        if (node != null) {
-            while (node.next != null) {
-                vals.add(node.val);
-                node = node.next;
-            }
-            // the last node
-            vals.add(node.val);
-        }
-        System.out.println(vals.toString());
-    }
+    // public static ListNode arrayToListNode(int[] nodeValues) {
+    //     ListNode dummyRoot = new ListNode(-1);
+    //     ListNode temp = dummyRoot;
+    //     for (int val : nodeValues) {
+    //         temp.next = new ListNode(val);
+    //         temp = temp.next;
+    //     }
+    //     return dummyRoot.next;
+    // }
+    //
+    // public static void print(ListNode node) {
+    //     ArrayList<Integer> vals = new ArrayList<>();
+    //     if (node != null) {
+    //         while (node.next != null) {
+    //             vals.add(node.val);
+    //             node = node.next;
+    //         }
+    //         // the last node
+    //         vals.add(node.val);
+    //     }
+    //     System.out.println(vals.toString());
+    // }
 }
 
