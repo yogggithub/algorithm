80bd1c8fd8cbf09bff7dff3fc32fe833ff394410
Eric Wu <ericwoodenman@gmail.com>
2021-11-10 07:56:50 -0500

    Solution for "543. Diameter of Binary Tree"

---
+++ LeetCode/Java/src/problems/p0543_DiameterOfBinaryTree/Self.java
@@ -0,0 +1,33 @@
+package problems.p0543_DiameterOfBinaryTree;
+
+import auxiliary.TreeNode;
+
+public class Self {
+    int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        /*
+         * Solution v1: Recursive & DFS
+         *
+         * Runtime: 0 ms @ (beats) 100%
+         * Memory Usage: 38.6 MB @ (beats) 94.62%
+         */
+        // idea
+        // for each node, the diameter is the sum of left and right sub trees
+        // And from the problem, the routine may not pass through root node
+        // Thus it is better to calculate max depth of every node
+        // at last return the maximum one
+        DFS(root);
+        return diameter;
+    }
+
+    private int DFS(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = DFS(root.left);
+        int right = DFS(root.right);
+        diameter = Math.max(diameter, left + right);
+        return Math.max(left, right) + 1;
+    }
+}
