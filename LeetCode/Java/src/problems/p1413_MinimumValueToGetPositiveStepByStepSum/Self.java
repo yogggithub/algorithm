4408df00f4a0a8ced5fb0f29bb24a499456d3d99
Eric Wu <ericwoodenman@gmail.com>
2021-11-11 07:13:25 -0500

    Solution for "1413. Minimum Value to Get Positive Step by Step Sum"

---
+++ LeetCode/Java/src/problems/p1413_MinimumValueToGetPositiveStepByStepSum/Self.java
@@ -0,0 +1,19 @@
+package problems.p1413_MinimumValueToGetPositiveStepByStepSum;
+
+public class Self {
+    public int minStartValue(int[] nums) {
+        /*
+         * Solution v1: Brute Force
+         *
+         * Runtime: 0 ms @ (beats) 100%
+         * Memory Usage: 36.5 MB @ (beats) 51.66%
+         */
+        int sum = 0, minimum = Integer.MAX_VALUE;
+        for (int num : nums) {
+            sum += num;
+            minimum = Math.min(sum, minimum);
+        }
+        // Minimum start value should be positive.
+        return minimum < 1 ? Math.abs(minimum) + 1 : 1;
+    }
+}
