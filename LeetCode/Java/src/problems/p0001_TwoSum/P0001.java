7783e2dce131f762ac9e279d5ae8dbe7682f5ce0
Eric Wu <ericwoodenman@gmail.com>
2021-11-11 10:10:27 -0500

    Move problem description to a separated md file.

--- LeetCode/Java/src/problems/p0001_TwoSum/P0001.java
+++ LeetCode/Java/src/problems/p0001_TwoSum/P0001.java
@@ -1,21 +1,5 @@
 package problems.p0001_TwoSum;
 
-/*
- * Two Sum
- * Given an array of integers, return indices of the two numbers
- * such that they add up to a specific target.
- * <p>
- * You may assume that each input would have exactly one solution,
- * and you may not use the same element twice.
- * <p>
- * <p>
- * Example:
- * Given nums = [2, 7, 11, 15], target = 9,
- * Because nums[0] + nums[1] = 2 + 7 = 9,
- * return [0, 1].
- */
-
-
 class P0001 {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
@@ -24,7 +8,7 @@ class P0001 {
                 if (nums[i] + nums[j] == target) {
                     result[0] = i;
                     result[1] = j;
-                    
+
                 }
             }
         }
